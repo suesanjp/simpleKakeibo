808395755978f1c88a8094325f1363326ccdbea9
Tomoaki Suzuki <suzuki.eplace@gmail.com>
2025-03-08 19:33:44 +0000

    production.rbに許可するホストを追加

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -77,10 +77,9 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  #
+  # 許可するホストを明示的に追加
+  config.hosts << "simplekakeibo-production.up.railway.app"
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
